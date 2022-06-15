6c8c3611dfd7ff272595f731d700a19d8c55d88e
Razhatawka <nataliia85sakhnova@gmail.com>
2022-06-15 18:14:15 +0300

    n

--- index.js
+++ index.js
@@ -9,17 +9,24 @@
 - В противном случае, полная дата в формате `"DD.MM.YY HH:mm"`.
 А именно: `"день.месяц.год часы:минуты"`,
 всё в виде двух цифр, т.е. `31.12.16 10:00`.
-alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
-alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
-alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
-// вчерашняя дата вроде 31.12.2016, 20:00
-alert( formatDate(new Date(new Date - 86400* 4 * 1000)) );
 
 
 const formatDate = (date) => {
-    const Now = new Date();
+    const now = new Date();
     diff = now - date;
     if (diff < 1)
+    alert( formatDate(new Date(new Date - 1)) );
+    console.log(Прямо сейчас);
+
+    alert( formatDate(new Date(new Date - 30 * 1000)) );
+    console.log(30 сек. назад);
+
+    alert( formatDate(new Date(new Date - 5 * 60 * 1000)) );
+    console.log(5 минут назад);
+    
+    alert( formatDate(new Date(new Date - 86400* 4 * 1000)) );
+    console.log(...Now);
+
 }
 
 
