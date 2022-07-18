794e00c8c3ae8de4d96420091e48eaddbcf1d031
Razhatawka <nataliia85sakhnova@gmail.com>
2022-07-18 13:57:43 +0300

    не работает

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const getTime = () => {
-    console.log('Yep!');
-    
-// const formatDate = (date) => {
-//     const diff = new Date() - date;
-//     console.log(diff);
-// }
+const formatDate = (date) => {
+        const diff = new Date() - date;
+        console.log(diff);
+        if (diff < 1000) {
+            console.log('Прямо сейчас');
+            // return
+            alert('Прямо сейчас');
+        } else if (diff < 60000) {
+            console.log(formatDate(new Date(new Date - 60000)), `${diff * 1000} + ${"секунд назад"}`);
+            // return
+            alert(`${diff * 1000} + ${"секунд назад"}`);
+        } else if (diff < 3600000) {
+            console.log(formatDate(new Date(new Date - 3600000)), `${diff * 60000} + ${"минут назад"}`);
+            // return
+            alert(`${diff * 60000} + ${"минут назад"}`);
+        } else {
+            console.log(`${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`);
+            // return
+            alert (`${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`);
+        }
+        // formatDate(new Date(new Date - 86400* 4 * 1000))
+    }
 
-// formatDate();
-
-// if (diff < 1000) {
-//     console.log(formatDate(new Date(new Date - 1000)), 'Прямо сейчас');
-// } else if (diff < 60000) {
-//     console.log(formatDate(new Date(new Date - 60000)), `${diff * 1000} + ${"секунд назад"}`);
-// } else if (diff < 3600000) {
-//     console.log(formatDate(new Date(new Date - 3600000)), `${diff * 60000} + ${"минут назад"}`);
-// } else {
-//     console.log(formatDate(new Date(new Date - 86400* 4 * 1000)), date);
-// }
-// formatDate(new Date(new Date - 86400* 4 * 1000))
-
-// console.log(date.getFullYear() + "." + date.getMonth() + "." + date.getDate() + " " + date.getHours() + ":" + date.getMinutes());
-}
-
-console.log(1)
-document.getElementById('button').onclick = () => getTime();
-// `${} + ${}`
+        document.getElementById('button').onclick = () => formatDate();
