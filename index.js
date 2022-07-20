303ee02de1e82e0c1e7382852ad6b636636e30ea
Razhatawka <nataliia85sakhnova@gmail.com>
2022-07-20 11:40:40 +0300

    d

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const formatDate = (date) => {
         console.log(diff);
         if (diff < 1000) {
             console.log('Прямо сейчас');
-            // return
-            alert('Прямо сейчас');
+            // alert('Прямо сейчас');
         } else if (diff < 60000) {
-            console.log(formatDate(new Date(new Date - 60000)), `${diff * 1000} + ${"секунд назад"}`);
-            // return
-            alert(`${diff * 1000} + ${"секунд назад"}`);
+            console.log(`${diff / 1000} секунд назад`);
+            // return alert(`${diff * 1000} + ${"секунд назад"}`);
         } else if (diff < 3600000) {
-            console.log(formatDate(new Date(new Date - 3600000)), `${diff * 60000} + ${"минут назад"}`);
+            console.log(`${Math.floor(diff / 60000)} минут назад`);
             // return
-            alert(`${diff * 60000} + ${"минут назад"}`);
+            // alert(`${diff * 60000} + ${"минут назад"}`);
         } else {
             console.log(`${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`);
             // return
-            alert (`${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`);
+            // alert (`${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`);
         }
     }
         
